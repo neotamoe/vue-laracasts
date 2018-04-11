be8f2969350c7206e390e931050c1ee850750ebc
neotamoe <neota.moe@gmail.com>
2018-04-11 14:50:31 -0500

    named slots

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-window.Event = new Vue();
 
-Vue.component('coupon', {
-    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
-    
-    methods: {
-        onCouponApplied() {
-            Event.$emit('applied');
-        }
-    }
+
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">
+                        <slot name="header"></slot>
+                    </p>
+                    <button class="delete" aria-label="close"></button>
+                </header>
+                <section class="modal-card-body">
+                    <slot>This slot content is hard coded here.  It will show up if there is no content in the modal
+                    component to overwrite it.</slot>
+                </section>
+                <footer class="modal-card-foot">
+                    <slot name="footer">
+                        <button class="button is-success">OKAY!</button>
+                    </slot>
+                </footer>
+            </div>
+        </div>
+    `,
 });
 
 
 new Vue({
     el: '#root',
-
-    data: {
-        couponApplied: false
-    },
-    methods: {
-        onCouponApplied() {
-            this.couponApplied = true;
-        }
-    },
-
-    created() {
-        Event.$on('applied', () => alert('Handling it!'));
-    }
 })
