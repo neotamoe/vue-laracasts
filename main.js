4ce0488aa3ff2c3e852f61c7254a9df34e8728df
neotamoe <neota.moe@gmail.com>
2018-04-11 13:07:02 -0500

    component within component

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks" :key="task.id">{{task.description}}</task>
+        </div>
+    `,
+    data() {
+        return {
+            tasks: [
+                { description: 'Go to the store', complete: true},
+                { description: 'Go to the bank', complete: false},
+                { description: 'Go to the farm', complete: true},
+                { description: 'Go to work', complete: false},
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
