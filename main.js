22bd37224eb0e13b6f547899a1e38600704d12d1
neotamoe <neota.moe@gmail.com>
2018-04-11 14:58:50 -0500

    inline templates

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
-
-
-Vue.component('modal', {
-    template: `
-        <div class="modal is-active">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-title">
-                        <slot name="header"></slot>
-                    </p>
-                    <button class="delete" aria-label="close"></button>
-                </header>
-                <section class="modal-card-body">
-                    <slot>This slot content is hard coded here.  It will show up if there is no content in the modal
-                    component to overwrite it.</slot>
-                </section>
-                <footer class="modal-card-foot">
-                    <slot name="footer">
-                        <button class="button is-success">OKAY!</button>
-                    </slot>
-                </footer>
-            </div>
-        </div>
-    `,
+Vue.component('progress-view', {
+    data () {
+        return { completionRate: 50 };
+    }
 });
 
 
