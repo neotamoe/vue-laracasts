ff160e66625a7a12d4b3c7ee9565ec9c2ffffc4c
neotamoe <neota.moe@gmail.com>
2018-04-11 14:35:41 -0500

    communicate between components

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+window.Event = new Vue();
+
 Vue.component('coupon', {
     template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
     
     methods: {
         onCouponApplied() {
-            this.$emit('applied');
+            Event.$emit('applied');
         }
     }
 });
@@ -11,9 +13,17 @@ Vue.component('coupon', {
 
 new Vue({
     el: '#root',
+
+    data: {
+        couponApplied: false
+    },
     methods: {
         onCouponApplied() {
-            alert('It was applied');
+            this.couponApplied = true;
         }
+    },
+
+    created() {
+        Event.$on('applied', () => alert('Handling it!'));
     }
 })
